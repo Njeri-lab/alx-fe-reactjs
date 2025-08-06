5cbc131d807270fbced5c4dc3524167f4b57cf9f
Joy Njeri <technjeri25@gmail.com>
2025-08-06 15:39:13 +0300

    Added inline css styles

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+export default function MainContent() {
+  return (
+    <main style={{ backgroundColor: "red" }}>
+      <p style={{ fontFamily: "fantasy" }}>
+        I love to visit New York, Paris, and Tokyo.
+      </p>
+    </main>
+  );
+}
---
+++ alx-react-app-new/src/components/WelcomeMessage.jsx
@@ -0,0 +1,12 @@
+function WelcomeMessage() {
+  return (
+    <div style={{ fontFamily: "sans-serif", textDecoration: "underline" }}>
+      <h1 style={{ fontSize: "45px" }}>
+        Hello everyone, I am learning React at ALX!
+      </h1>
+      <p style={{ color: "brown" }}>I am learning about JSX!</p>
+    </div>
+  );
+}
+
+export default WelcomeMessage;
