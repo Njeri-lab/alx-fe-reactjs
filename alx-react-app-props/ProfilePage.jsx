77513c4997aa995903857651d1b751b714c8b226
Joy Njeri <technjeri25@gmail.com>
2025-08-07 16:03:03 +0300

    moved

--- alx-react-app-props/ProfilePage.jsx
+++ alx-react-app-props/ProfilePage.jsx
@@ -1,6 +1,9 @@
+import { useContext } from "react";
 import UserInfo from "./UserInfo";
+import UserContext from "./UserContext";
 
-function ProfilePage({ userData }) {
+function ProfilePage() {
+  const useData = useContext(UserContext);
   return <UserInfo userData={userData} />;
 }
 
