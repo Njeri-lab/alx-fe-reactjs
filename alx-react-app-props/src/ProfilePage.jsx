d2a4c74c99dffd55a565bab1d33ea43b0448b39b
Joy Njeri <technjeri25@gmail.com>
2025-08-07 13:05:51 +0300

    Prop drilling using Context API exercise

---
+++ alx-react-app-props/src/ProfilePage.jsx
@@ -0,0 +1,11 @@
+import React, { useContext } from "react";
+import { UserContext } from "./UserContext";
+import UserInfo from "./UserInfo";
+
+function ProfilePage() {
+  const userData = useContext(UserContext);
+
+  return <UserInfo userData={userData} />;
+}
+
+export default ProfilePage;
