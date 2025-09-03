63c8bf4aeff153dc7240199feaf58d45585ec60f
Joy Njeri <technjeri25@gmail.com>
2025-09-03 16:43:08 +0300

    responsive design

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,10 +1,10 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg sm:p-4 md:p-8 sm:max-w-xs">
+    <div className="bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="rounded-full w-36 h-36 mx-auto sm:w-24,h-24 md:w-26,h-26"
+        className="rounded-full w-36 h-36 mx-auto sm:w-24 sm:h-24 md:w-26 md:h-26"
       />
       <h1 className="text-xl text-blue-800 my-4 sm:text-lg md:text-xl">
         John Doe
