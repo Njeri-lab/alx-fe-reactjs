baf57732a20ecfdeb1ec21137576c429362f0ac1
Joy Njeri <technjeri25@gmail.com>
2025-09-03 15:56:53 +0300

    Initial commit

---
+++ github-user-search/github-user-search/src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Router>
+          <Route></Route>
+        </Router>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
