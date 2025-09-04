d2992b3de0526acaef238b1e1461bfb657b97c73
Joy Njeri <technjeri25@gmail.com>
2025-09-04 08:18:44 +0300

    validation

--- form-handling-react/src/components/formikForm.jsx
+++ form-handling-react/src/components/formikForm.jsx
@@ -1,12 +1,24 @@
 import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+const validationSchema = Yup.object({
+  name: Yup.string().required("Name is required"),
+  email: Yup.string().email("Invalid email").required("Email is required"),
+});
 
 const formikForm = () => {
-  <Formik initialValues={{ name: "", email: "", password: "" }}>
+  <Formik
+    initialValues={{ name: "", email: "", password: "" }}
+    validationSchema={validationSchema}
+  >
     {() => (
       <Form>
         <Field type="text" name="username" />,
+        <ErrorMessage name="name" component="div" />
         <Field type="email" name="email" />,
+        <ErrorMessage name="email" component="div" />
         <Field type="password" name="password" />
+        <ErrorMessage name="password" component="div" />
         <button type="submit">Submit</button>
       </Form>
     )}
