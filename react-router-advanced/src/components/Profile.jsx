1b3ea4efc20b1f9459f5f5e7db37e018c48489e0
Joy Njeri <technjeri25@gmail.com>
2025-09-04 09:17:18 +0300

    commit

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,11 +1,23 @@
-import { Link, Outlet } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom";
+import ProfileDetails from "./ProfileDetails";
+import ProfileSettings from "./ProfileSettings";
 
 function Profile() {
   return (
     <>
-      <Link to="details">Details</Link>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route path="details" element={<ProfileDetails />} />
+            <Route path="settings" element={<ProfileSettings />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+
+      {/* <Link to="details">Details</Link>
       <Link to="settings">Settings</Link>
       <Outlet />
+      */}
     </>
   );
 }
