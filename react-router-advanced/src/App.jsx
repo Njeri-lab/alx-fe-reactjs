23321b82a07fa5426d5e7ba56ba6d77b77c259df
Joy Njeri <technjeri25@gmail.com>
2025-09-04 09:20:55 +0300

    :l:

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
             <Route path="settings" element={<ProfileSettings />} />
           </Route>
 
-          <Route path="blog/:Id" element={<BlogPost />} />
+          <Route path="/blog/:Id" element={<BlogPost />} />
         </Routes>
       </Router>
     </BrowserRouter>
