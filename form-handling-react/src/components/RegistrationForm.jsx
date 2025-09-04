723e54ea8f2700adc793b8471f886c4273a4e54f
Joy Njeri <technjeri25@gmail.com>
2025-09-04 08:12:31 +0300

    basic validation

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -5,6 +5,27 @@ const RegistrationForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!username || !email) {
+      alert("Must fill in the blanks");
+    }
+
+    if (password.length < 8) {
+      alert("Passwprd must be at least 8 characters long");
+    }
+
+    if (!/\d/.test(password)) {
+      alert("Password must contain at least one number");
+    }
+
+    if (!/[!@#$%^&*]/.test(password)) {
+      alert("Password must contain at ;east one special character");
+    }
+
+    alert("Form submitted successfully");
+  };
   return (
     <>
       <input
@@ -24,6 +45,10 @@ const RegistrationForm = () => {
         value={password}
         onChange={(e) => setPassword(e.target.value)}
       />
+
+      <button type="submit" onClick={handleSubmit}>
+        Submit
+      </button>
     </>
   );
 };
