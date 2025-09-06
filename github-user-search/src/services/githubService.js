8e496642bf3ba13ff44cf981ea4911db4bc1c81e
Joy Njeri <technjeri25@gmail.com>
2025-09-06 18:53:32 +0300

    commit

---
+++ github-user-search/src/services/githubService.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const url = "https://api.github.com/users/{username}";
+export async function fetchUserData(username) {
+  try {
+    const response = await axios.get(`${url}/${username}`);
+    return response.data;
+  } catch (error) {
+    if (error.response && error.respose.status === 404) {
+      throw new Error("User not found");
+    }
+    throw new Error("Failed to fetch user data");
+  }
+}
